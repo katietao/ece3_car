98db187f0d26ddfa3810afea23e424ec6210ce7b
Katie Tao <113798691+katietao@users.noreply.github.com>
2026-04-30 11:16:23 -0700

    first commit

---
+++ ece3_car.ino
@@ -0,0 +1,16 @@
+const int left_nslp_pin  = 31;  // nslp = not sleep --> set high --> digitalWrite
+const int left_dir_pin   = 29;  // control direction of motor --> digitalWrite
+const int left_pwm_pin   = 40;  // use to control motor speed --> analogWrite
+const int right_nslp_pin = 11;
+const int right_dir_pin  = 30;
+const int right_pwm_pin  = 39;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
